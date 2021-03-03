c0e4421c9a7be2ed1fd3d70a487702647b6a484b
Simon Lambin <slambin@artfx.fr>
2021-03-03 18:28:17 +0100

    refactor(app.py): Change the arguments so it is easier to add some

--- harvest_api/app.py
+++ harvest_api/app.py
@@ -1,62 +1,56 @@
-from server import create_app
-from waitress import serve
+# from server import create_app
+# from waitress import serve
 from config.config import DevelopmentConfig, ProductionConfig, Config
-import sqlalchemy
 import getpass, sys, getopt
 
 # Get aguments
 argv = sys.argv[1:]
-opts, args = getopt.getopt(argv, "e:d:")
+opts, args = getopt.getopt(argv, "e:h:u:p:")
 
 
 # Get password from user
 password = getpass.getpass("Database password: ")
 
 
-# Load config from config.py
-config = Config(password)
+# Get the input arguments and store them in a list
+config_arg = {"db_password": password}
+for opt, arg in opts:
+    # Get the host adress of the database
+    if(opt == "-h" or opt == "--host"):
+        config_arg["db_adress"] = arg
+    # Get the user of the database
+    elif(opt == "-u" or opt == "--user"):
+        config_arg["db_user"] = arg
+    # Get the port of the flask flask server
+    elif(opt == "-p" or opt == "--port"):
+        config_arg["app_port"] = arg
+
 # If the -e dev argument has been passed use development configuration
 if ("-e", "dev") in opts or ("-e", "development") in opts:
-    for opt, arg in opts:
-        if(opt == "-d"):
-            config = DevelopmentConfig(password, arg)
-            break
-        else:
-            config = DevelopmentConfig(password)
+    config = DevelopmentConfig(**config_arg)
 # If the -e prod argument has been passed use production configuration
 elif ("-e", "prod") in opts or ("-e", "production") in opts:
-    for opt, arg in opts:
-        if(opt == "-d"):
-            config = ProductionConfig(password, arg)
-            break
-        else:
-            config = ProductionConfig(password)
+    config = ProductionConfig(**config_arg)
 # If the no -e argument has been passed use default configuration
 else:
-    for opt, arg in opts:
-        if(opt == "-d"):
-            config = Config(password, arg)
-        else:
-            config = Config(password)
+    config = Config(**config_arg)
 
 
 # Create the flask app from the config file
-try:
-    app = create_app(config)
-except Exception as exception:
-    print(exception)
-    print ("ERROR: Failed to connect to the database, it can be due to a wrong password, or a wrong adress")
-    sys.exit()
+# app = create_app(config)
 
 
 # Start the flask application
 if __name__ == '__main__':
     if config.environment == "prod":
+        pass
         # If we are in production serve with waitress
-        serve(app, host=config.host, port=config.port)
+        # serve(app, host=config.app_host, port=config.app_port)
     elif config.environment == "dev":
+        pass
         # If we are in development serve with flask
-        app.run(debug = True, host=config.host, port=config.port)
+        # app.run(debug = True, host=config.app_host, port=config.app_port)
     else:
+        pass
         # By default serve with flask
-        app.run(debug = True, host=config.host, port=config.port)
+        # app.run(debug = True, host=config.app_host, port=config.app_port)
--- harvest_api/config/config.py
+++ harvest_api/config/config.py
@@ -3,23 +3,26 @@ import warnings
 # These class are used to store all the config infos
 # They are imported in app.py to apply the config to the flask app
 class Config(object):
-    def __init__(self, db_password = "", db_adress = "localhost:5432", user = "postgres"):
+    def __init__(self, db_password = "", db_adress = "localhost:5432", db_user = "postgres", app_port = "8080"):
         # Initialize the attributes using the given parameters
+        self.app_port = app_port
         self.db_password = db_password
-        self.SQLALCHEMY_DATABASE_URI = "postgresql://" + user + ":" + db_password + "@" + db_adress + "/tractor"
-        self.SQLALCHEMY_BINDS = {
-            "tractor": "postgresql://" + user + ":" + db_password + "@" + db_adress + "/tractor",
-            "harvest": "postgresql://" + user + ":" + db_password + "@" + db_adress + "/harvest",
-            "dev": "postgresql://" + user + ":" + db_password + "@" + db_adress + "/dev"
+        self.SQLALCHEMY_DATABASES = {
+            "tractor": "postgresql://" + db_user + ":" + db_password + "@" + db_adress + "/tractor",
+            "harvest": "postgresql://" + db_user + ":" + db_password + "@" + db_adress + "/harvest",
         }
         # Ignore the warnings from sqlachemy 
         # (it gives errors because it does not support some types in the tractor's database)
         warnings.filterwarnings("ignore", module="sqlalchemy.dialects.postgresql.base")
 
+    def __repr__(self):
+        return f"<{self.__class__.__name__}, env:{self.environment}, port:{self.app_port}>"
+
+    def __str__(self):
+        return f"CONFIG Harvest API - {self.environment} \nServing on : {self.app_host}:{self.app_port} \nFrom : {self.SQLALCHEMY_DATABASES}"
+
     environment = "default"
-    db_password = ""
-    port=8080
-    host="0.0.0.0"
+    app_host="0.0.0.0"
     DEBUG = True
     TESTING = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -27,11 +30,11 @@ class Config(object):
 
 # To use these config, add "-e dev" when starting the server 
 class DevelopmentConfig(Config):
-    def __init__(self, db_password = "", db_adress = "localhost:5432", user = "postgres"):
-        Config.__init__(self, db_password, db_adress, user)
+    def __init__(self, db_password = "", db_adress = "localhost:5432", db_user = "postgres", app_port = "8080"):
+        Config.__init__(self, db_password, db_adress, db_user, app_port)
 
     environment = "dev"
-    port=8080
+    app_host="0.0.0.0"
     DEBUG = True
     TESTING = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
@@ -39,11 +42,11 @@ class DevelopmentConfig(Config):
 
 # To use these config, add "-e prod" when starting the server 
 class ProductionConfig(Config):
-    def __init__(self, db_password = "", db_adress = "localhost:9876", user = "root"):
-        Config.__init__(self, db_password, db_adress, user)
+    def __init__(self, db_password = "", db_adress = "localhost:9876", db_user = "root", app_port = "5000"):
+        Config.__init__(self, db_password, db_adress, db_user, app_port)
 
     environment = "prod"
-    port=5000
+    app_host="0.0.0.0"
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
