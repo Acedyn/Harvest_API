511050661f3e3acdc7d528b9ae8ed871ceb010be
Simon Lambin <simon.lamb98@gmail.com>
2021-03-04 16:30:07 +0100

    feat(validation.py): Get the last validation date from query

--- harvest_api/routes/validation.py
+++ harvest_api/routes/validation.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask import jsonify
-from database import execute_from_file
+from database import execute_from_file, sessions
+from mappings.harvest_tables import Project
+import re
 
 ########################################
 ## Validation blueprint : Set of routes that will return and get data
@@ -13,13 +15,23 @@ validation = Blueprint("validation", __name__)
 # Route for "/validation/unvalidated-progression/<project>"
 @validation.route("/validation/unvalidated-progression/<project>")
 def unvalidated_progression(project):
-    # TODO: Get the validation date from the harvest database
+    # Query the last validation date of the given parameter
+    last_validation = sessions["harvest"].query(Project.last_validation).filter(Project.name == project.upper()).first()
+
     # Set the parameters from the project name and the last validation date
-    parameters = ({"project": project, "last_validation": "2021-02-26 20:14:22"})
+    parameters = ({"project": project.upper(), "last_validation": last_validation[0].strftime("%Y-%m-%d %H:%M:%S")})
     # Query from the unvalidated_frames.sql file, with the parameters
     results = execute_from_file("tractor", "unvalidated_frames.sql", parameters)
 
-    for result in results:
-        print(result)
+    # Initialize the final response that will contain all the frames
+    response = []
 
-    return "OK"
+    # Loop over all the result to store them in the response
+    for result in results:
+        # Cast the result to the right type
+        sequence = int(re.sub("[^0-9]", '', result["sequence"]))
+        shot = int(re.sub("[^0-9]", '', result["shot"]))
+        # Append the casted result
+        response.append({"sequence": sequence, "shot": shot, "frame": result["frame"]})
+
+    return jsonify(response)
