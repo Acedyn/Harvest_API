21554dd7edf721f2fb4984eb37eec5437ac1196a
Simon Lambin <slambin@artfx.fr>
2021-03-14 21:54:01 +0100

    feat(infos.py): Return the projects infos

--- harvest_api/routes/infos.py
+++ harvest_api/routes/infos.py
@@ -15,12 +15,22 @@ combine_filters = (
     Sequence.project_id == Project.id,
 )
 
+# Retrieves the list of the project and multiple infos about them
 @infos.route("/infos/projects")
 def project_infos():
-    # Get all the project and the amount of frames to compute
+    # Get all the project, their color and the amount of frames to compute
     projects_query = select([Project.name, Project.color, func.count(1).label("total")]) \
         .where(and_( \
         *combine_filters)). \
-        group_by(Project.name)
+        group_by(Project.name, Project.color)
     # Execute the query
     results = engines["harvest"].execute(projects_query)
+
+    # Initialize the final response that will contain project
+    response = []
+    
+    # Convert the sql result to a jsonifyable list
+    for result in results:
+        response.append({"name": result["name"], "color": result["color"], "frames": result["total"]})
+    
+    return jsonify(response)
