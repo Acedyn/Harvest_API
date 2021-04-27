f44001efc1f0ad0dd9e4e79bfc357927373cd4a3
Simon Lambin <slambin@artfx.fr>
2021-04-27 12:59:52 +0200

    Take the valid entry when validating entities

--- harvest_api/config/config.py
+++ harvest_api/config/config.py
@@ -3,63 +3,102 @@ import warnings
 # These class are used to store all the config infos
 # They are imported in app.py to apply the config to the flask app
 class Config(object):
-    def __init__(self, db_password = "", db_adress = "localhost:5432", db_user = "postgres", app_port = "8080"):
+    def __init__(
+        self,
+        db_password="",
+        db_adress="localhost:5432",
+        db_user="postgres",
+        app_port="8080",
+    ):
         # Initialize the attributes using the given parameters
         self.app_port = app_port
         self.db_password = db_password
         self.SQLALCHEMY_DATABASES = {
-            "tractor": "postgresql://" + db_user + ":" + db_password + "@" + db_adress + "/tractor",
-            "harvest": "postgresql://" + db_user + ":" + db_password + "@" + db_adress + "/harvest2",
+            "tractor": "postgresql://"
+            + db_user
+            + ":"
+            + db_password
+            + "@"
+            + db_adress
+            + "/tractor",
+            "harvest": "postgresql://"
+            + db_user
+            + ":"
+            + db_password
+            + "@"
+            + db_adress
+            + "/harvest2",
         }
-        # Ignore the warnings from sqlachemy 
+        # Ignore the warnings from sqlachemy
         # (it gives errors because it does not support some types in the tractor's database)
         warnings.filterwarnings("ignore", module="sqlalchemy.dialects.postgresql.base")
 
     def __repr__(self):
-        return f"<{self.__class__.__name__}, env:{self.environment}, port:{self.app_port}>"
+        return (
+            f"<{self.__class__.__name__}, env:{self.environment}, port:{self.app_port}>"
+        )
 
     def __str__(self):
         return f"CONFIG Harvest API - {self.environment} \nServing on : {self.app_host}:{self.app_port} \nFrom : {self.SQLALCHEMY_DATABASES}"
 
     environment = "default"
-    app_host="0.0.0.0"
+    app_host = "0.0.0.0"
     # TODO: Fix the scheduler so it does not start another scheduler every time the app reload
     DEBUG = False
     TESTING = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-# To use these config, add "-e dev" when starting the server 
+# To use these config, add "-e dev" when starting the server
 class DevelopmentConfig(Config):
-    def __init__(self, db_password = "", db_adress = "localhost:5432", db_user = "postgres", app_port = "8080"):
+    def __init__(
+        self,
+        db_password="",
+        db_adress="localhost:5432",
+        db_user="postgres",
+        app_port="8080",
+    ):
         Config.__init__(self, db_password, db_adress, db_user, app_port)
 
     environment = "dev"
-    app_host="0.0.0.0"
+    app_host = "0.0.0.0"
     # TODO: Fix the scheduler so it does not start another scheduler every time the app reload
     DEBUG = False
     TESTING = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    
 
-# To use these config, add "-e prod" when starting the server 
+
+# To use these config, add "-e prod" when starting the server
 class ProductionConfig(Config):
-    def __init__(self, db_password = "", db_adress = "localhost:9876", db_user = "root", app_port = "5000"):
+    def __init__(
+        self,
+        db_password="",
+        db_adress="localhost:9876",
+        db_user="root",
+        app_port="5000",
+    ):
         Config.__init__(self, db_password, db_adress, db_user, app_port)
 
     environment = "prod"
-    app_host="0.0.0.0"
+    app_host = "0.0.0.0"
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# To use these config, add "-e docker" when starting the server 
+
+# To use these config, add "-e docker" when starting the server
 class DockerConfig(Config):
-    def __init__(self, db_password = "", db_adress = "localhost:9876", db_user = "root", app_port = "5000"):
+    def __init__(
+        self,
+        db_password="",
+        db_adress="localhost:9876",
+        db_user="root",
+        app_port="5000",
+    ):
         Config.__init__(self, db_password, db_adress, db_user, app_port)
 
     environment = "prod"
-    app_host="0.0.0.0"
+    app_host = "0.0.0.0"
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- harvest_api/routes/validation.py
+++ harvest_api/routes/validation.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from sqlalchemy import func, case, not_
-from sqlalchemy.sql import select, and_, true, false
+from sqlalchemy.sql import select, and_
 from database import execute_from_file, sessions, engines
 from mappings.harvest_tables import Project, Sequence, Shot, Frame, Layer
 import re, datetime
@@ -144,6 +144,8 @@ def validate_progression_layer(project):
             url.pop(0)
 
             new_data = {}
+            new_data["valid"] = item["valid"]
+
             if(len(url) >= 1):
                 new_data["sequence"] = url[0]
             if(len(url) >= 2):
@@ -170,16 +172,18 @@ def validate_progression_layer(project):
 
 
     current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
 
     # Try to query the update of the databse according sequence_data
     try:
-        sessions["harvest"].query(Layer.valid)\
-        .filter(*combine_filters)\
-        .filter(Project.name == re.sub("-", '_', project.upper()))\
-        .filter(Sequence.index.in_([item["sequence"] for item in sequence_data]))\
-        .update({Layer.valid: not_(Layer.valid), Layer.validation_date: current_datetime}, synchronize_session = False)
+        for item in sequence_data:
+            sessions["harvest"].query(Layer.valid)\
+            .filter(*combine_filters)\
+            .filter(Project.name == re.sub("-", '_', project.upper()))\
+            .filter(Sequence.index == item["sequence"])\
+            .update({Layer.valid: item["valid"], Layer.validation_date: current_datetime}, synchronize_session = False)
 
-        sessions["harvest"].commit()
+            sessions["harvest"].commit()
 
     # If an error occured return an error message
     except Exception as exception:
@@ -188,14 +192,15 @@ def validate_progression_layer(project):
 
     # Try to query the update of the databse according shot_data
     try:
-        sessions["harvest"].query(Layer.valid)\
-        .filter(*combine_filters)\
-        .filter(Project.name == re.sub("-", '_', project.upper()))\
-        .filter(Shot.index.in_([item["shot"] for item in shot_data]))\
-        .filter(Sequence.index.in_([item["sequence"] for item in shot_data]))\
-        .update({Layer.valid: not_(Layer.valid), Layer.validation_date: current_datetime}, synchronize_session = False)
+        for item in shot_data:
+            sessions["harvest"].query(Layer.valid)\
+            .filter(*combine_filters)\
+            .filter(Project.name == re.sub("-", '_', project.upper()))\
+            .filter(Shot.index == item["shot"])\
+            .filter(Sequence.index == item["sequence"])\
+            .update({Layer.valid: item["valid"], Layer.validation_date: current_datetime}, synchronize_session = False)
 
-        sessions["harvest"].commit()
+            sessions["harvest"].commit()
 
     # If an error occured return an error message
     except Exception as exception:
@@ -204,15 +209,16 @@ def validate_progression_layer(project):
 
     # Try to query the update of the databse according frame_data
     try:
-        sessions["harvest"].query(Layer.valid)\
-        .filter(*combine_filters)\
-        .filter(Project.name == re.sub("-", '_', project.upper()))\
-        .filter(Frame.index.in_([item["frame"] for item in frame_data]))\
-        .filter(Shot.index.in_([item["shot"] for item in frame_data]))\
-        .filter(Sequence.index.in_([item["sequence"] for item in frame_data]))\
-        .update({Layer.valid: not_(Layer.valid), Layer.validation_date: current_datetime}, synchronize_session = False)
+        for item in shot_data:
+            sessions["harvest"].query(Layer.valid)\
+            .filter(*combine_filters)\
+            .filter(Project.name == re.sub("-", '_', project.upper()))\
+            .filter(Frame.index == item["frame"])\
+            .filter(Shot.index == item["shot"])\
+            .filter(Sequence.index == item["sequence"])\
+            .update({Layer.valid: item["valid"], Layer.validation_date: current_datetime}, synchronize_session = False)
 
-        sessions["harvest"].commit()
+            sessions["harvest"].commit()
 
     # If an error occured return an error message
     except Exception as exception:
@@ -221,16 +227,17 @@ def validate_progression_layer(project):
     
     # Try to query the update of the databse according layer_data
     try:
-        sessions["harvest"].query(Layer.valid)\
-        .filter(*combine_filters)\
-        .filter(Project.name == re.sub("-", '_', project.upper()))\
-        .filter(Layer.name.in_([item["layer"] for item in layer_data]))\
-        .filter(Frame.index.in_([item["frame"] for item in layer_data]))\
-        .filter(Shot.index.in_([item["shot"] for item in layer_data]))\
-        .filter(Sequence.index.in_([item["sequence"] for item in layer_data]))\
-        .update({Layer.valid: not_(Layer.valid), Layer.validation_date: current_datetime}, synchronize_session = False)
+        for item in shot_data:
+            sessions["harvest"].query(Layer.valid)\
+            .filter(*combine_filters)\
+            .filter(Project.name == re.sub("-", '_', project.upper()))\
+            .filter(Layer.name == item["layer"])\
+            .filter(Frame.index == item["frame"])\
+            .filter(Shot.index == item["shot"])\
+            .filter(Sequence.index == item["sequence"])\
+            .update({Layer.valid: item["valid"], Layer.validation_date: current_datetime}, synchronize_session = False)
 
-        sessions["harvest"].commit()
+            sessions["harvest"].commit()
 
     # If an error occured return an error message
     except Exception as exception:
