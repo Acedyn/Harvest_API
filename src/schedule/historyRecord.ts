8cf5402ad758ed3b3b590680970962e165125141
Simon Lambin <slambin@artfx.fr>
2022-01-23 23:17:00 +0100

    Await in the startDataRecord

--- src/schedule/historyRecord.ts
+++ src/schedule/historyRecord.ts
@@ -2,8 +2,8 @@ import { createProjectRecord } from "../db/project";
 import { createBladeRecord } from "../db/blade";
 
 export function startDataRecord(queryInterval: number, storeInterval: number) {
-  setInterval(() => {
-    // createProjectRecord("TEST_PIPE", 4);
-    createBladeRecord({busy: 2, free: 5, nimby: 29, off: 21});
-  }, queryInterval);
+  setInterval(async () => {
+    await createProjectRecord("TEST_PIPE", 4);
+    await createBladeRecord({busy: 2, free: 5, nimby: 29, off: 21});
+  }, storeInterval);
 }
