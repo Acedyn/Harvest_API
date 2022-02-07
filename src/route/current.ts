bcb906470424a07837002b55406ed8a04f75c3b9
Joseph Henry <josephenry@protonmail.com>
2022-02-07 11:35:49 +0100

    refactor: remove response types

--- src/route/current.ts
+++ src/route/current.ts
@@ -1,16 +1,16 @@
-import { Request, Response, Application } from "express";
+import { Application } from "express";
 
 import { getBladeUsage } from "../query/blade";
 import { getProjectUsage } from "../query/project";
 
 export function getCurrentBladeUsage(app: Application) {
-  app.get("/current/blade-usage", async (req: Request, res: Response) => {
+  app.get("/current/blade-usage", async (req, res) => {
     res.send(await getBladeUsage());
   });
 }
 
 export function getCurrentProjectUsage(app: Application) {
-  app.get("/current/project-usage", async (req: Request, res: Response) => {
+  app.get("/current/project-usage", async (req, res) => {
     res.send(await getProjectUsage());
   });
 }
--- src/route/info.ts
+++ src/route/info.ts
@@ -1,12 +1,13 @@
-import { Request, Response, Application } from "express";
+import { Application } from "express";
 
 import { getProjectNames } from "../db/project";
 import { getProjectComputeTime } from "../db/project";
+import { queryBlades } from "../query/blade";
 import { RequestQuery } from "../types/api";
 import { getTimeRange } from "../utils/time";
 
 export function getProjects(app: Application) {
-  app.get("/info/projects", async (req: Request, res: Response) => {
+  app.get("/info/projects", async (req, res) => {
     res.send(await getProjectNames());
   });
 }
@@ -16,18 +17,26 @@ export function getComputeTime(app: Application) {
     "/info/compute-time",
     async (
       req: RequestQuery<{ start?: string; end?: string; project?: string }>,
-      res: Response
+      res
     ) => {
       const [start, end] = getTimeRange(req);
+
       const totalComputeTime = await getProjectComputeTime(
         new Date(start),
         new Date(end),
         req.query.project
       );
+
       res.send({
         hours: Math.floor(totalComputeTime.getTime() / 1000 / 60 / 60),
         minutes: totalComputeTime.getMinutes(),
       });
     }
   );
+}
+
+export function getBlades(app: Application) {
+  app.get("/info/blades", async (req, res) => {
+    res.send((await queryBlades()).data);
+  });
 }
