80faa2e2d3128f2f7998a3e67d14c6be9d2e46cf
Joseph Henry <josephenry@protonmail.com>
2022-02-07 11:35:28 +0100

    refactor: plain hour

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 
-import { startDataRecord } from "./schedule/historyRecord";
+import { startDataRecord } from "./schedule/history";
 import { startRestServer } from "./route/app";
 
 dotenv.config();
 
 startRestServer(3000);
-startDataRecord(60000, 60000 * 60);
+startDataRecord(60000);
