f5a5b32e223f61a02906150576caaf8f82e58e4e
Simon Lambin <simon.lamb98@gmail.com>
2022-05-18 12:17:29 +0200

    Revert "Add clear no free slots behaviour"

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 
 import { startDataRecord } from "./schedule/history";
-import { startClearNoFreeSlots } from "./schedule/nofreeslots";
 import { initializeRoutes, startRestServer } from "./route/app";
 
 // Read env file for configuration
@@ -10,4 +9,3 @@ dotenv.config();
 initializeRoutes();
 startRestServer(3000);
 startDataRecord(60000);
-startClearNoFreeSlots(60000 * 60 * 3)
--- src/query/blade.ts
+++ src/query/blade.ts
@@ -1,37 +1,11 @@
 import { Blade } from "../types/tractor";
 import { tractorQuery } from "../utils/tractor";
+
 /**
  * Queries Tractor about blades
  */
 export function queryBlades() {
   return tractorQuery<{ blades: Blade[] }>("monitor?q=blades");
-}
-
-export async function getNoFreeSlots() {
-  const bladesResponse = await queryBlades();
-
-  if (!bladesResponse) {
-    return [];
-  }
-
-  const noFreeSlotBlades : Blade[] = [];
-
-  bladesResponse.data.blades.forEach((blade: Blade) => {
-    const lastPulse = new Date(blade.t * 1000);
-
-    // Exclude all busy blade
-    if (blade.owners && blade.owners.length) {
-      return;
-    }
-
-    // blade is active
-    if (Date.now() - lastPulse.getTime() < 500000) {
-      if (blade.note !== "no free slots (1)") return;
-      noFreeSlotBlades.push(blade);
-      return
-    }
-  });
-  return noFreeSlotBlades;
 }
 
 export async function getBladeUsage() {
@@ -47,7 +21,7 @@ export async function getBladeUsage() {
   let offCount = 0;
   let noFreeSlotsCount = 0;
 
-  bladesResponse.data.blades.forEach((blade: Blade) => {
+  bladesResponse.data.blades.forEach((blade) => {
     const lastPulse = new Date(blade.t * 1000);
 
     // The blade is busy
